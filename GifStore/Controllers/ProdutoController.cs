8917833cb18f862be1a466d2f754a1b7aaee7219
IGOR PEREIRA DUTRA <igordutra07@hotmail.com>
2020-12-04 19:58:39 -0400

    feito o cadastro de produtos

---
+++ GifStore/Controllers/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using GifStore.Data;
+using GifStore.Historias.Produto.Cadastrar;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GifStore.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly DataContexto contexto;
+
+        public ProdutoController(DataContexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            return View(contexto.Produtos.ToList());
+        }
+        public IActionResult Cadastrar() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(
+            [FromServices] CadastrarProduto cadastrarProduto,
+            CadastrarProdutoViewModel cadastrarProdutoVm)
+        {
+            if (!ModelState.IsValid)
+                return View(cadastrarProdutoVm);
+
+            await cadastrarProduto.Executar(cadastrarProdutoVm);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ GifStore/Historias/Produto/Cadastrar/CadastrarProdutoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GifStore.Data.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace GifStore.Historias.Produto.Cadastrar
+{
+    public class CadastrarProdutoViewModel
+    {
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Status do produto")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        public StatusDoProduto Status { get; set; }
+
+        
+    }
+}
